4bf84e42812c9cb14e0f3f6d2145ba35258d51d1
Francisco Borja Lobato <fjbl2788@gmail.com>
2022-12-21 07:36:31 -0500

    Use bootstrap

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import _ from 'lodash';
-import './style.css';
+import './style.scss';
+import * as bootstrap from 'bootstrap'
+import logo from './images/Tadeo.png';
+import carousel from './images/Adviento.jpeg';
+import carousel2 from './images/Adviento2.jpeg';
+
+const headLogo = document.getElementById('logo');
+const myLogo = new Image();
+myLogo.src = logo;
+headLogo.appendChild(myLogo);
+
+const carouselOne = document.getElementById('carousel-item');
+const myCarousel = new Image();
+myCarousel.src = carousel;
+carouselOne.appendChild(myCarousel);
+carouselOne.classList.add('d-block w-100');
+
+const carouselTwo = document.getElementById('carousel-item-one');
+const myCarouselTwo = new Image();
+myCarouselTwo.src = carousel2;
+carouselTwo.appendChild(myCarouselTwo);
+carouselTwo.classList.add('d-block w-100');
+
+const carouselThree = document.getElementById('carousel-item-two');
+const myCarouselThree = new Image();
+myCarouselThree.src = logo;
+carouselThree.appendChild(myCarouselThree);
+carouselThree.classList.add('d-block w-100');
 
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
   return element;
 }
